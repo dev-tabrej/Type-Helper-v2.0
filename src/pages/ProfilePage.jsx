1e573521a47aa6fd057cac95d4d7f31ad8f05153
dev-tabrej <dev.tabrej@gmail.com>
2024-07-01 17:45:01 +0530

    Profile page complete

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,53 +3,73 @@ import { Text } from "@chakra-ui/react";
 import React from "react";
 
 function ProfilePage() {
-  const array = [1, 2, 3, 4, 5, 6];
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <Flex w={"full"} mx={5} gap={2}>
+    <Flex w={"full"} borderRadius={5}>
       <Flex
         direction={"column"}
         align={"center"}
+        borderRadius={5}
         flex={20}
-        borderRight={"2px solid black"}
+        height={400}
+        boxShadow={"0px 0px 4px black"}
         p={2}
       >
         <Avatar size={"2xl"} />
         <Text fontSize={"2xl"} m={2}>
           Tabrej
         </Text>
+        <Flex direction={"column"} mt={10} justify={"center"} align={"center"}>
+          <Text>Your Personal Best</Text>
+          <Box>
+            <Text>Typing type</Text>
+            <Text>Accuracy</Text>
+            <Text>WPM</Text>
+          </Box>
+        </Flex>
       </Flex>
-      <Flex flex={80} direction={"Column"}>
+      <Flex
+        flex={80}
+        direction={"column"}
+        mx={2}
+        borderRadius={5}
+        maxHeight={"full"}
+        boxShadow={"0px 0px 4px black"}
+      >
         <Flex
-          // flex={100}
-          height={100}
+          height={20}
           fontWeight={"bolder"}
           fontSize={"xl"}
           justify={"space-around"}
           align={"center"}
           border={"2px solid black"}
-          mx={5}
         >
           <Text>S No</Text>
           <Text>Typing type</Text>
           <Text>Accuracy</Text>
           <Text>WPM</Text>
         </Flex>
-        {array.map((i) => (
-          <Flex
-            key={i}
-            // flex={100}
-            height={100}
-            justify={"space-around"}
-            align={"center"}
-            border={"2px solid black"}
-            mx={5}
-          >
-            <Text>{i}</Text>
-            <Text>Typing type</Text>
-            <Text>Accuracy</Text>
-            <Text>WPM</Text>
-          </Flex>
-        ))}
+        <Box overflowY={"scroll"} maxHeight={580} mt={2}>
+          {array.map((i) => (
+            <Flex
+              key={i}
+              height={20}
+              justify={"space-around"}
+              align={"center"}
+              bg={"rgba(90,95,95,0.5)"}
+              // border={"2px solid black"}
+              boxShadow={"0px 0px 2px black"}
+              borderRadius={2}
+              m={2}
+              color={"Skyblue"}
+            >
+              <Text>{i}</Text>
+              <Text>Programming</Text>
+              <Text>88.37 %</Text>
+              <Text>28</Text>
+            </Flex>
+          ))}
+        </Box>
       </Flex>
     </Flex>
   );
