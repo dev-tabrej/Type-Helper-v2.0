169e7e40acd4d9b25f91d8f970a03336deff3949
dev-tabrej <dev.tabrej@gmail.com>
2024-07-29 16:07:41 +0530

    theme changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,36 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
+import { mode } from "@chakra-ui/theme-tools";
+import { extendTheme, ColorModeScript } from "@chakra-ui/react";
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("grey.800", "whitealpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#101010",
+  },
+};
+const theme = extendTheme({ colors, config, styles });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RecoilRoot>
-          <App />
+          <App/>
         </RecoilRoot>
       </ChakraProvider>
     </BrowserRouter>
